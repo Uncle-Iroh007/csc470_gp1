8228be124eaf8b5e0a217aa4e3f7e01c1c684c6d
1999ajf <anna.fields@trojans.dsu.edu>
2020-08-25 14:00:42 -0500

    Added Hello World text box and adjusted sizing

--- P1 Code/FormHelloWorld.Designer.cs
+++ P1 Code/FormHelloWorld.Designer.cs
@@ -19,7 +19,7 @@
             }
             base.Dispose(disposing);
         }
-
+        
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -29,37 +29,51 @@
         private void InitializeComponent()
         {
             this.ClickMe = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // ClickMe
             // 
-            this.ClickMe.Location = new System.Drawing.Point(296, 243);
-            this.ClickMe.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ClickMe.Location = new System.Drawing.Point(478, 304);
+            this.ClickMe.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.ClickMe.Name = "ClickMe";
-            this.ClickMe.Size = new System.Drawing.Size(99, 36);
+            this.ClickMe.Size = new System.Drawing.Size(176, 56);
             this.ClickMe.TabIndex = 0;
             this.ClickMe.Text = "Exit";
             this.ClickMe.UseVisualStyleBackColor = true;
             this.ClickMe.Click += new System.EventHandler(this.ClickMe_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(20, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(680, 153);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Hello World";
+            // 
             // FormHelloWorld
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(439, 317);
+            this.ClientSize = new System.Drawing.Size(784, 428);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.ClickMe);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "FormHelloWorld";
             this.Text = "CSC 470 - P1";
             this.Load += new System.EventHandler(this.FormHelloWorld_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button ClickMe;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
